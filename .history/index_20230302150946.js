2b0a0d2afeaef74c8a3ec287c83ef43be5bdb729
leonardo mendoza <leo7mendoza7@gmail.com>
2023-03-02 15:13:23 -0300

    first commit

---
+++ .history/index_20230302150946.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+/* import productsRoutes from "./routes/products.js";
+import stripeRoute from "./routes/stripe.js"; */
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.send("Home");
+});
+
+/* app.use("/products", productsRoutes);
+app.use("/stripe", stripeRoute); */
+
+app.listen(process.env.PORT, () =>
+  console.log("Servidor levantado en ${process.env.PORT}.env.PORT")
+);
---
+++ .history/products/products_20230302151116.js
@@ -0,0 +1,160 @@
+const products = [
+  {
+    Nombre: "Zapatillas Nike Zoom Winflo 8",
+    Precio: 42.899,
+    Imagen:
+      "https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw343c2c59/products/NI_CW3419-010/NI_CW3419-010-1.JPG",
+    Descripcion:
+      "Para los amantes del running y la velocidad, las zapatillas Nike Zoom Winflo 8 son perfectas. Porque acompañan tu ritmo dándote amortiguación en cada pisada. Su diseño de confección es alucinante: se combina con una parte superior traslúcida y la tecnología Flywire que brinda un ajuste estable y cómodo para las carreras largas. Además, su tela de malla asegura ventilación constante, manteniendo tus pies frescos en todo momento. La unidad Zoom Air en el antepié proporciona amortiguación adicional y junto a la espuma Cushlon Foam hacen que cada paso sea suave y brinde respuesta inmediata entregando a la vez, suma elasticidad. Elegí la cantidad de kilómetros que quieras, ellas están preparadas.",
+    Genero: " Hombre",
+    Id: 1,
+    Adecuado: "Running",
+    Material: "Mesh",
+    Beneficios: " Amortiguación",
+  },
+  {
+    Nombre: "Zapatillas Nike Zoomx Vaporfly Next 2",
+    Precio: 97.499,
+    Imagen:
+      "https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwab369e58/products/NI_CU4123-101/NI_CU4123-101-1.JPG",
+    Descripcion:
+      "Gana potencia en cada carrera con las Zapatillas Nike Zoomx Vaporfly Next% 2, diseñadas con espuma Nike ZoomX, que aporta un retorno de energía superior, y una placa de fibra de carbono que favorece el movimiento constante. Para mejorar la ventilación y la estabilidad su capellada se construyó con malla y refuerzos a lo largo del antepié. Buscando mejorar la amortiguación, cuentan con una cápsula de espuma interna en el talón y su suela con ranuras flexibles optimizan la tracción en distintos terrenos y condiciones meteorológicas.",
+    Genero: "Hombre",
+    Id: 2,
+    Adecuado: "Correr",
+    Material: "Malla",
+    Beneficios: " Amortiguación",
+  },
+  {
+    Nombre: "Zapatillas Nike Lebron XIX",
+    Precio: 84.499,
+    Imagen:
+      "https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwa9cb2d99/products/NI_DC9339-001/NI_DC9339-001-1.JPG",
+    Descripcion:
+      "Las Zapatillas Nike Lebron XIX Low tienen un diseño extremo y detalles que las vuelven un fuego. La unidad Max Air visible se combina con una unidad Zoom Air gruesa y ofrecen amortiguación extra para que des todo de vos en tus prácticas y competencias. La entresuela de espuma Cushlon es suave y estable, y brinda una respuesta inmediata. El material de la capellada es resistente y el cuello y lengüeta acolchados mejora la sensación para que disfrutes de cada momento en tus partidos, con total comodidad, y te luzcas como lo haría Lebron.",
+    Genero: "Unisex",
+    Id: 3,
+    Adecuado: "Entrenamiento",
+    Material: "Sintético",
+    Beneficios: "Amortiguación",
+  },
+  {
+    Nombre: "Zapatillas Nike Court Legacy",
+    Precio: 29.899,
+    Imagen:
+      "https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw3f5dd18f/products/NI_DH3161-100/NI_DH3161-100-1.JPG",
+    Descripcion:
+      "Las Zapatillas Nike Court Legacy honra la historia arraigada en el mundo del tenis, un básico probado a través del tiempo. Su capellada construida en cuero rugoso con costuras tradicionales les aporta mayor durabilidad y comodidad, el swoosh retro también te permite combinarlas con tus jeans o remeras favoritas. La suela de goma con patrón de espiguilla te da mejor tracción en diferentes terrenos y su entresuela de espuma te brinda una amortiguación inigualable en cada paso. Además, la famosa pipa en la capellada te da toda la onda característica de Nike.",
+    Genero: "Hombre",
+    Id: 4,
+    Adecuado: "Todo el día",
+    Material: "Cuero sintético",
+    Beneficios: "Durabilidad",
+  },
+  {
+    Nombre: "Zapatillas Nike Zoom Prevail",
+    Precio: 42.899,
+    Imagen:
+      "https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw92983832/products/NI_DA1102-004/NI_DA1102-004-1.JPG",
+    Descripcion:
+      "Las Zapatillas Nike Zoom Prevail cuentan con un diseño ligero y de gran soporte. La unidad Zoom Air se ubica en el antepié y combina durabilidad y ligereza gracias a que se fusiona con la espuma Nike React. La malla de la capellada aporta transpirabilidad y la espuma adicional en la lengüeta brinda mayor suavidad y hace que sea más fácil ponerlas y quitarlas. La suela de goma tiene una almohadilla de protección contra impactos angulada y brinda toda la tracción que necesitas en tu rutina.",
+    Genero: "Hombre",
+    Id: 5,
+    Adecuado: "Correr",
+    Material: "Malla",
+    Beneficios: "Amortiguación",
+  },
+  {
+    Nombre: "Zapatillas Nike React Infinity 2",
+    Precio: 52.999,
+    Imagen:
+      "https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw80dcf37f/products/NI_CT2357-005/NI_CT2357-005-1.JPG",
+    Descripcion:
+      "Las Nike React Infinity 2 son zapatillas diseñadas para correr de forma segura evitando lesiones. Son especiales para correr maratones ya que contiene una entresuela adaptada para el movimiento y tienen una base más ancha que las anteriores versiones, garantizando mayor estabilidad en cada pisada. Este calzado tiene una capellada Flyknit Loft que permite un ajuste firme y que perdura muchos kilómetros al contar con tres capas de malla transpirable. Ahora correr es mucho más fácil con estas zapatillas.",
+    Genero: "Hombre",
+    Id: 6,
+    Adecuado: "Running",
+    Material: "Poliéster",
+    Beneficios: "Resistencia",
+  },
+  {
+    Nombre: "Zapatillas Nike Air Max Ap",
+    Precio: 45.499,
+    Imagen:
+      "https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw4107cbc1/products/NI_DQ5023-121/NI_DQ5023-121-1.JPG",
+    Descripcion:
+      "Las Zapatillas Nike Air Max AP tienen un diseño deportivo y elegante que mezcla pasado y presente para ofrecer una comodidad excepcional. Los detalles con toques de herencia rinden tributo al Air Max 97. Su capellada es de mesh y su suela de goma. Tienen diseño de perfil bajo con cuello acolchado y suave, y plantilla cómoda perfecto para llevar con cualquier conjunto.",
+    Genero: "Hombre",
+    Id: 7,
+    Adecuado: "Entrenamiento",
+    Material: "Mesh",
+    Beneficios: "Amortiguación",
+  },
+  {
+    Nombre: "Zapatillas Nike Wildhorse 7",
+    Precio: 51.999,
+    Imagen:
+      "https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw752f2084/products/NI_CW3421-103/NI_CW3421-103-1.JPG",
+    Descripcion:
+      "Las zapatillas Nike Wildhorse 7 son zapatillas todo terreno. Porque garantizan durabilidad para carreras largas y caminos difíciles. La amortiguación de espuma Nike React de la entresuela aporta capacidad de respuesta inmediata para cada paso que das ofeciendo una pisada suave y elástica. Además, la malla transpirable en su parte superior, aporta ventilación, haciendo tu jornada de running mucho más fresca, seca y confortable. Las ranuras de tracción multidireccionales de su suela, ayudan a proporcionar un mejor agarre en tramos de subidas o bajadas. Con estas zapatillas, te convertirás en el corredor más eficiente.",
+    Genero: "Hombre",
+    Id: 8,
+    Adecuado: "Running",
+    Material: "Mesh",
+    Beneficios: "Amortiguación",
+  },
+  {
+    Nombre: "Zapatillas Nike Downshifter 12",
+    Precio: 29.899,
+    Imagen:
+      "https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwbae14382/products/NI_DD9293-400/NI_DD9293-400-1.JPG",
+    Descripcion:
+      "Las Zapatillas Nike Downshifter 12 son perfectas para tus días de running, la malla superior te brinda comodidad transpirable, con ranuras flexibles en la suela te da mejor tracción, reacción, y amortiguación en cada pisada, además el logotipo swoosh metálico le añade un toque deportivo y original.",
+    Genero: "Hombre",
+    Id: 9,
+    Adecuado: "Correr",
+    Material: "Mix de materiales Hi",
+    Beneficios: "Amortiguación",
+  },
+  {
+    Nombre: "Zapatillas Nike Zoom Fly 4",
+    Precio: 68.899,
+    Imagen:
+      "https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw8fef0331/products/NI_CT2392-005/NI_CT2392-005-1.JPG",
+    Descripcion:
+      "Hechas para durar y ayudarte a volar en lugar de correr, las Zapatillas Nike Zoom Fly 4 son la continuación de un calzado que brinda todo lo mejor que Nike tiene para ofrecer a los runners más exigentes. La tela con tecnología Flyknit mejora la sujeción y hace que sea más fácil ponerlas y quitarlas, además de generar una sensación de comodidad superior para tus carreras. Cuentan con tecnología Nike React, basada en una espuma ligera que mejora la respuesta. La malla de la capellada aporta transpirabilidad y tiene refuerzos que brindan una mayor estabilidad para que sumes kilómetros en cada salida, alcanzando todo el confort posible.",
+    Genero: "Hombre",
+    Id: 10,
+    Adecuado: "Correr",
+    Material: "Malla",
+    Beneficios: "Amortiguación",
+  },
+  {
+    Nombre: "Zapatillas Nike Court Vision Mid",
+    Precio: 35.099,
+    Imagen:
+      "https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw3ae0ac6b/products/NI_DH2987-105/NI_DH2987-105-1.JPG",
+    Descripcion:
+      "Las Zapatillas Nike Court Vision Mid son dueñas de un look clásico del básquetbol de los años 80, pero renovadas. Con una parte superior intensa y de revestimientos cosidos, están inspiradas en los ganchos del básquet de la vieja escuela, mientras que el cuello super suave de perfil bajo agrega un look elegante que se siente super cómodo en su uso diario.",
+    Genero: "Hombre",
+    Id: 11,
+    Adecuado: "Todo el día",
+    Material: "Sintético",
+    Beneficios: " Durabilidad",
+  },
+  {
+    Nombre: "Zapatillas Nike Revolution 6",
+    Precio: 25.999,
+    Imagen:
+      "https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw2ba64150/products/NI_DC3728-005/NI_DC3728-005-1.JPG",
+    Descripcion:
+      "Pasos amortiguados, una estética cuidada y una pisada más flexible; todo esto es posible con las Zapatillas Nike Revolution 6, diseñadas con una capellada de malla para mejorar la transpirabilidad y una suela que aporta tracción en cada carrera. La malla acolchada se extiende a lo largo del cuello para suavizar el contacto y las capas adicionales aportan durabilidad. La entresuela de espuma brinda un confort único y te ayuda a seguir siempre hacia adelante.",
+    Genero: "Hombre",
+    Id: 12,
+    Adecuado: " Correr",
+    Material: "Malla",
+    Beneficios: "Amortiguación",
+  },
+];
+
+export default products;
